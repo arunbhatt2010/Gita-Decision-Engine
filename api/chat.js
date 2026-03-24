0bb86bae2b300bc1e8dead9bcb75b1ba357c9553
arunbhatt2010 <69510836+arunbhatt2010@users.noreply.github.com>
2026-03-24 18:02:35 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "llama3-70b-8192",
+        model: "llama3-8b-8192",
         messages: [
           {
             role: "system",
